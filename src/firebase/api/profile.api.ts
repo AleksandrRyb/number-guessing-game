23e89ece688bca1b96ccbf218d42f4a50aec2b79
Aleksandr Rybin <a_lv_l@bk.ru>
2021-06-12 20:04:07 +1000

    Изменил название переменных firebase на более логичные

--- src/firebase/api/profile.api.ts
+++ src/firebase/api/profile.api.ts
@@ -4,33 +4,34 @@ import { firebaseApp } from "../init";
 const db = firebaseApp.firestore();
 
 export async function getProfile(user: firebase.User) {
-  //try to get existed profile if it exists
-  const existed = await db
+  const snapshot = await db
     .collection("profiles")
     .where("userId", "==", user.uid)
     .get();
 
-  if (existed.size !== 0) {
+  if (snapshot.size !== 0) {
     const profile = {
-      id: existed.docs[0].id,
-      ...existed.docs[0].data(),
+      id: snapshot.docs[0].id,
+      ...snapshot.docs[0].data(),
     };
 
     return profile;
   }
 
-  // if in not, create new profile
   const { uid, displayName, photoURL, email } = user;
-  const created = await db.collection("profiles").add({
+  const documentRef = await db.collection("profiles").add({
     userId: uid,
     name: displayName,
     avatar: photoURL,
-    created: firebase.firestore.FieldValue.serverTimestamp(),
+    documentRef: firebase.firestore.FieldValue.serverTimestamp(),
     email: email,
     wins: 0,
     loses: 0,
   });
-  const newProfile = { id: created.id, ...(await created.get()).data() };
+  const newProfile = {
+    id: documentRef.id,
+    ...(await documentRef.get()).data(),
+  };
   return newProfile;
 }
 
